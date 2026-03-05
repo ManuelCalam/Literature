d76eb2969a1dd899cb9fcd58c8a0c059fecce6e5
ManuelCalam <jmanuelc369@gmail.com>
2026-03-05 16:22:46 -0600

    DTO's and JsonServiceParser

--- src/main/java/com/alura/literature/LiteratureApplication.java
+++ src/main/java/com/alura/literature/LiteratureApplication.java
@@ -1,13 +1,35 @@
 package com.alura.literature;
 
+import com.alura.literature.client.BookApiClient;
+import com.alura.literature.dto.GutendexResponseDTO;
+import com.alura.literature.service.JsonParserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LiteratureApplication {
+public class LiteratureApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		SpringApplication.run(LiteratureApplication.class, args);
 	}
 
+    @Override
+    public void run(String... args) {
+
+        BookApiClient client = new BookApiClient();
+        JsonParserService parser = new JsonParserService();
+
+        String url = "https://gutendex.com/books/";
+
+        String json = client.fetchBooks(url);
+
+        GutendexResponseDTO response =
+                parser.parse(json, GutendexResponseDTO.class);
+
+        response.results().forEach(book ->
+                System.out.println(book.title())
+        );
+
+    }
 }
---
+++ src/main/java/com/alura/literature/service/JsonParserService.java
@@ -0,0 +1,19 @@
+package com.alura.literature.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+public class JsonParserService {
+
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    public <T> T parse(String json, Class<T> clazz) {
+
+        try {
+            return mapper.readValue(json, clazz);
+        } catch (Exception e) {
+            throw new RuntimeException("Error parsing JSON", e);
+        }
+
+    }
+}
